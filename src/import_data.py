2173df71ca4a49b8c8bbc83460c4ad480c5f897e
Samuel-Levesque <samuel-levesque@hotmail.com>
2018-11-10 10:12:45 -0500

    Finished import data functions

--- src/import_data.py
+++ src/import_data.py
@@ -5,25 +5,16 @@ Created on Sat Nov 10 09:33:22 2018
 
 @author: Samuel_Levesque
 """
-import csv
-import os
+import numpy as np
 
 
 def import_training_data():
-    my_path = os.path.abspath(os.path.dirname(__file__))
-    path = os.path.join(my_path, "data/train-features.csv")
-    with open(path, 'rb') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',')
-        for row in spamreader:
-            print(', '.join(row))
+    X_full_data = np.genfromtxt("../data/train-features.csv", delimiter=",", skip_header=True)
+    y_full_data = np.genfromtxt("../data/data-id-train.csv", delimiter=",", skip_header=True)
 
-    return 0
+    return X_full_data, y_full_data
 
 
 if __name__ == "__main__":
-#    test = import_training_data()
-
-    my_path = os.path.abspath(os.path.dirname(__file__))
-    path = os.path.join(my_path, "data/train_features.csv")
-    with open(path) as f:
-        test = list(csv.reader(f))
+    X, y = import_training_data()
+    print(X, y)
--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 from import_data import import_training_data
-from sklearn import train_test_split
+#from sklearn import train_test_split
 
 
 def main():
     X_full_data, y_full_data = import_training_data()
 
+    print(X_full_data, y_full_data)
+
     # Separation of full dataset
-    X_train, X_test, y_train, y_test = train_test_split(X_full_data, y_full_data, test_size = 0.2)
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size = 0.2)
+    #X_train, X_test, y_train, y_test = train_test_split(X_full_data, y_full_data, test_size = 0.2)
+    #X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size = 0.2)
 
 
 
